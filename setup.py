ac7e996b2a12270e5853e597412dece96c27c6f5
spl <simon@raspberrypi.org>
2015-04-17 10:50:14 +0100

    Initial check-in of vanilla code.

---
+++ setup.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+### BEGIN LICENSE
+# Copyright (C) 2010 Leszek Lesner leszek@zevenos.com
+# This program is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License version 3, as published 
+# by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranties of 
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+# PURPOSE.  See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along 
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+### END LICENSE
+
+###################### DO NOT TOUCH THIS (HEAD TO THE SECOND PART) ######################
+
+import os
+import sys
+
+try:
+    import DistUtilsExtra.auto
+except ImportError:
+    print >> sys.stderr, 'To build lxkeymap you need https://launchpad.net/python-distutils-extra'
+    sys.exit(1)
+assert DistUtilsExtra.auto.__version__ >= '2.18', 'needs DistUtilsExtra.auto >= 2.18'
+
+def update_data_path(prefix, oldvalue=None):
+
+    try:
+        fin = file('lxkeymap/lxkeymapconfig.py', 'r')
+        fout = file(fin.name + '.new', 'w')
+
+        for line in fin:            
+            fields = line.split(' = ') # Separate variable from value
+            if fields[0] == '__lxkeymap_data_directory__':
+                # update to prefix, store oldvalue
+                if not oldvalue:
+                    oldvalue = fields[1]
+                    line = "%s = '%s'\n" % (fields[0], prefix)
+                else: # restore oldvalue
+                    line = "%s = %s" % (fields[0], oldvalue)
+            fout.write(line)
+
+        fout.flush()
+        fout.close()
+        fin.close()
+        os.rename(fout.name, fin.name)
+    except (OSError, IOError), e:
+        print ("ERROR: Can't find lxkeymap/lxkeymapconfig.py")
+        sys.exit(1)
+    return oldvalue
+
+
+def update_desktop_file(datadir):
+
+    try:
+        fin = file('lxkeymap.desktop.in', 'r')
+        fout = file(fin.name + '.new', 'w')
+
+        for line in fin:            
+            if 'Icon=' in line:
+                line = "Icon=%s\n" % (datadir + 'media/icon.png')
+            fout.write(line)
+        fout.flush()
+        fout.close()
+        fin.close()
+        os.rename(fout.name, fin.name)
+    except (OSError, IOError), e:
+        print ("ERROR: Can't find lxkeymap.desktop.in")
+        sys.exit(1)
+
+
+class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
+    def run(self):
+        previous_value = update_data_path(self.prefix + '/share/lxkeymap/')
+        update_desktop_file(self.prefix + '/share/lxkeymap/')
+        DistUtilsExtra.auto.install_auto.run(self)
+        update_data_path(self.prefix, previous_value)
+
+
+        
+##################################################################################
+###################### YOU SHOULD MODIFY ONLY WHAT IS BELOW ######################
+##################################################################################
+
+DistUtilsExtra.auto.setup(
+    name='lxkeymap',
+    version='0.1',
+    license='GPL-3',
+    #author='Your Name',
+    #author_email='email@ubuntu.com',
+    #description='UI for managing …',
+    #long_description='Here a longer description',
+    #url='https://launchpad.net/lxkeymap',
+    cmdclass={'install': InstallAndUpdateDataDirectory}
+    )
+
